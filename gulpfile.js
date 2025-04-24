2ae0968eb6775aa4427f18f6ece904b7edd75af0
mbschranz <mbschranz@gmail.com>
2025-04-23 23:49:18 -0400

    implemented most of gulpfile.js
    added most of the functions for gulpfile.js EXCEPT for the build and default functions which gave me issues. NOTE will ask professor about it.

---
+++ gulpfile.js
@@ -0,0 +1,112 @@
+const { src, dest, series, watch } = require(`gulp`),
+    htmlValidator = require(`gulp-html`),
+    htmlCompressor = require(`gulp-htmlmin`),
+    cssValidator = require(`gulp-stylelint`),
+    cssCompressor = require(`gulp-clean-css`),
+    jsLinter = require(`gulp-eslint`),
+    babel = require(`gulp-babel`),
+    jsCompressor = require(`gulp-uglify`),
+    browserSync = require(`browser-sync`),
+    reload = browserSync.reload;
+
+let validateHTML = () => {
+    return src(`app/html/index.html`)
+        .pipe(htmlValidator());
+};
+
+let compressHTML = () => {
+    return src(`app/html/index.html`)
+        .pipe(htmlCompressor({collapseWhitespace:true}))
+        .pipe(dest(`prod/html`));
+};
+
+let validateCSS = () => {
+    return src(`app/css/style.css`)
+        .pipe(cssValidator({
+            failAfterError: false,
+            reporters: [{
+                formatter: `string`,
+                console: true
+            }]
+        }));
+};
+
+let compressCSS = () => {
+    return src(`app/css/style.css`)
+        .pipe(cssCompressor())
+        .pipe(dest(`prod/css`));
+};
+
+let validateJS = () => {
+    return src(`app/js/app.js`)
+        .pipe(jsLinter())
+        .pipe(jsLinter.formatEach(`compact`));
+};
+
+let compressJS = () => {
+    return src(`app/js/app.js`)
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/js`));
+};
+
+let transpileJSForDev = () => {
+    return src(`app/js/app.js`)
+        .pipe(babel())
+        .pipe(dest(`temp/js`));
+};
+
+let transpileJSForProd = () => {
+    return src(`app/js/app.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/js`));
+};
+
+let serve = () => {
+    browserSync({
+        notify: true,
+        reloadDelay: 50,
+        server: {
+            baseDir: [
+                `./`,
+                `temp`
+            ]
+        }
+    });
+    watch(`app/js/app.js`, series(jsLinter, transpileJSForDev))
+        .on(`change`, reload);
+
+    watch(`app/css/style.css`, compressCSS)
+        .on(`change`, reload);
+
+    watch(`app/html/index.html`, validateHTML)
+        .on(`change`, reload);
+};
+
+let build = () => {
+    compressHTML();
+    compressCSS();
+    transpileJSForProd();
+};
+
+//exports.default = build; does not work yet
+exports.validateHTML = validateHTML;
+exports.compressHTML = compressHTML;
+exports.validateCSS = validateCSS;
+exports.compressCSS = compressCSS;
+exports.validateJS = validateJS;
+exports.compressJS = compressJS;
+exports.transpileJSForDev = transpileJSForDev;
+exports.transpileJSForProd = transpileJSForProd;
+exports.serve = series(
+    validateHTML,
+    validateCSS,
+    transpileJSForDev,
+    serve
+);
+exports.build = series(
+    compressHTML,
+    compressCSS,
+    transpileJSForProd
+);
+
